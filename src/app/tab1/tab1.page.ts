98f8155139c40aae48cd73e941de776585435864
Juan Agapito <jcagapitoz@gmail.com>
2022-12-12 19:38:33 -0500

    Fin Sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  numero:number = 0;
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.numero = 10;
+    //debugger;
+    this.numero = 20;
+    this.numero+= 15;
+    console.log(this.numero);
+    console.log("Hola Mundo");
+  }
 
 }
